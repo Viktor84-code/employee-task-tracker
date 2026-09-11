9a3ee390d15f0747c8f3934b0303ad2e102103e5
Your Name <you@example.com>
2026-09-11 16:38:40 +0300

    Fixed API URLs for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import { useAuthStore } from './stores/auth'
 
-export const AUTH_API = 'http://localhost:8002'
-export const TASK_API = 'http://localhost:8003'
+export const AUTH_API = 'http://158.160.231.42:8002'
+export const TASK_API = 'http://158.160.231.42:8003'
 
 async function parseError(response) {
   let data = {}
